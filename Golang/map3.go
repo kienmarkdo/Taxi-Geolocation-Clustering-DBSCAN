82cb6540734ae5cfefee67d645c5da2d305ccacb
Kien Do <kienmarkdo@gmail.com>
2022-02-28 13:59:26 -0500

    Restart with map3.go

--- Golang/map3.go
+++ Golang/map3.go
@@ -1,96 +1,94 @@
-// Project CSI2120/CSI2520 
+// Project CSI2120/CSI2520
 // Winter 2022
 // Robert Laganiere, uottawa.ca
 
 package main
 
-import "fmt"
-import "time"
-import "os"
-import "io"
-import "strconv"
-import "encoding/csv"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"time"
+)
 
 type GPScoord struct {
-
-    lat float64
+	lat  float64
 	long float64
 }
 
 type LabelledGPScoord struct {
-    GPScoord
-	ID int     // point ID
-	Label int  // cluster ID
+	GPScoord
+	ID    int // point ID
+	Label int // cluster ID
 }
 
-const N int=4
-const MinPts int=5
-const eps float64= 0.0003
-const filename string="yellow_tripdata_2009-01-15_9h_21h_clean.csv"
-
+const N int = 4
+const MinPts int = 5
+const eps float64 = 0.0003
+const filename string = "yellow_tripdata_2009-01-15_9h_21h_clean.csv"
 
 func main() {
 
-    start := time.Now(); 
+	start := time.Now()
 
-    gps, minPt,maxPt := readCSVFile(filename)
+	gps, minPt, maxPt := readCSVFile(filename)
 	fmt.Printf("Number of points: %d\n", len(gps))
-	
-	minPt= GPScoord{-74., 40.7}
-	maxPt= GPScoord{-73.93,40.8}
-	
+
+	minPt = GPScoord{-74., 40.7}
+	maxPt = GPScoord{-73.93, 40.8}
+
 	// geographical limits
-    fmt.Printf("NW:(%f , %f)\n", minPt.long, minPt.lat)
-    fmt.Printf("SE:(%f , %f) \n\n", maxPt.long, maxPt.lat)
-	
-	
+	fmt.Printf("NW:(%f , %f)\n", minPt.long, minPt.lat)
+	fmt.Printf("SE:(%f , %f) \n\n", maxPt.long, maxPt.lat)
+
 	// Parallel DBSCAN STEP 1.
-	incx := (maxPt.long-minPt.long)/float64(N)
-	incy := (maxPt.lat-minPt.lat)/float64(N)
-	
-	var grid [N][N][]LabelledGPScoord  // a grid of GPScoord slices
-	
+	incx := (maxPt.long - minPt.long) / float64(N)
+	incy := (maxPt.lat - minPt.lat) / float64(N)
+
+	var grid [N][N][]LabelledGPScoord // a grid of GPScoord slices
+
 	// Create the partition
 	// triple loop! not very efficient, but easier to understand
-	
-	partitionSize:=0
-    for j:=0; j<N; j++ {
-        for i:=0; i<N; i++ {
-		
-		    for _, pt := range gps {
-			
-			    // is it inside the expanded grid cell
-			    if (pt.long >= minPt.long+float64(i)*incx-eps) && (pt.long < minPt.long+float64(i+1)*incx+eps) && (pt.lat >= minPt.lat+float64(j)*incy-eps) && (pt.lat < minPt.lat+float64(j+1)*incy+eps) {
-				
-                    grid[i][j]= append(grid[i][j], pt) // add the point to this slide
-					partitionSize++;
-                }				
+
+	partitionSize := 0
+	for j := 0; j < N; j++ {
+		for i := 0; i < N; i++ {
+
+			for _, pt := range gps {
+
+				// is it inside the expanded grid cell
+				if (pt.long >= minPt.long+float64(i)*incx-eps) && (pt.long < minPt.long+float64(i+1)*incx+eps) && (pt.lat >= minPt.lat+float64(j)*incy-eps) && (pt.lat < minPt.lat+float64(j+1)*incy+eps) {
+
+					grid[i][j] = append(grid[i][j], pt) // add the point to this slide
+					partitionSize++
+				}
 			}
-	    }
+		}
 	}
-	
+
 	// ***
 	// This is the non-concurrent procedural version
 	// It should be replaced by a producer thread that produces jobs (partition to be clustered)
 	// And by consumer threads that clusters partitions
-    for j:=0; j<N; j++ {
-        for i:=0; i<N; i++ {
-		
-		    DBscan(grid[i][j], MinPts, eps, i*10000000+j*1000000)
+	for j := 0; j < N; j++ {
+		for i := 0; i < N; i++ {
+
+			DBscan(grid[i][j], MinPts, eps, i*10000000+j*1000000)
 		}
 	}
-	
+
 	// Parallel DBSCAN STEP 2.
 	// Apply DBSCAN on each partition
 	// ...
-	
-	
+
 	// Parallel DBSCAN step 3.
 	// merge clusters
 	// *DO NOT PROGRAM THIS STEP
-	
-	end := time.Now();
-    fmt.Printf("\nExecution time: %s of %d points\n", end.Sub(start), partitionSize)
+
+	end := time.Now()
+	fmt.Printf("\nExecution time: %s of %d points\n", end.Sub(start), partitionSize)
 }
 
 // Applies DBSCAN algorithm on LabelledGPScoord points
@@ -100,107 +98,107 @@ func main() {
 // returns number of clusters found
 func DBscan(coords []LabelledGPScoord, MinPts int, eps float64, offset int) (nclusters int) {
 
-   // *** fake code: to be rewritten
-   time.Sleep(3)
-   nclusters=0
-   for i, pt := range coords {
-   
-	  if (i==10) {
-		 nclusters++
-	  }
- 	  if (i==100) {
-		 nclusters++
-	  }
- 	  if (i==100) {
-	     break
-	  }
-	  
-      pt.Label= offset+nclusters
-   }
-   // *** end of fake code.
-   
-   // End of DBscan function
-   // Printing the result (do not remove)
-   fmt.Printf("Partition %10d : [%4d,%6d]\n", offset, nclusters, len(coords))
-   
-   return nclusters
+	// *** fake code: to be rewritten
+	time.Sleep(3)
+	nclusters = 0
+	for i, pt := range coords {
+
+		if i == 10 {
+			nclusters++
+		}
+		if i == 100 {
+			nclusters++
+		}
+		if i == 100 {
+			break
+		}
+
+		pt.Label = offset + nclusters
+	}
+	// *** end of fake code.
+
+	// End of DBscan function
+	// Printing the result (do not remove)
+	fmt.Printf("Partition %10d : [%4d,%6d]\n", offset, nclusters, len(coords))
+
+	return nclusters
 }
 
-// reads a csv file of trip records and returns a slice of the LabelledGPScoord of the pickup locations  
+// reads a csv file of trip records and returns a slice of the LabelledGPScoord of the pickup locations
 // and the minimum and maximum GPS coordinates
 func readCSVFile(filename string) (coords []LabelledGPScoord, minPt GPScoord, maxPt GPScoord) {
 
-    coords= make([]LabelledGPScoord, 0, 5000)
+	coords = make([]LabelledGPScoord, 0, 5000)
 
-    // open csv file
-    src, err := os.Open(filename)
+	// open csv file
+	src, err := os.Open(filename)
 	defer src.Close()
-    if err != nil {
-        panic("File not found...")
-    }
-	
+	if err != nil {
+		panic("File not found...")
+	}
+
 	// read and skip first line
-    r := csv.NewReader(src)
-    record, err := r.Read()
-    if err != nil {
-        panic("Empty file...")
-    }
+	r := csv.NewReader(src)
+	record, err := r.Read()
+	if err != nil {
+		panic("Empty file...")
+	}
 
-    minPt.long = 1000000.
-    minPt.lat = 1000000.
-    maxPt.long = -1000000.
-    maxPt.lat = -1000000.
-	
-	var n int=0
-	
-    for {
-        // read line
-        record, err = r.Read()
+	minPt.long = 1000000.
+	minPt.lat = 1000000.
+	maxPt.long = -1000000.
+	maxPt.lat = -1000000.
 
-        // end of file?
-        if err == io.EOF {
-            break
-        }
+	var n int = 0
 
-        if err != nil {
-             panic("Invalid file format...")
-        }
-		
+	for {
+		// read line
+		record, err = r.Read()
+
+		// end of file?
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			panic("Invalid file format...")
+		}
+
 		// get lattitude
 		lat, err := strconv.ParseFloat(record[8], 64)
-        if err != nil {
-		     fmt.Printf("\n%d lat=%s\n",n,record[8])
-             panic("Data format error (lat)...")
-        }
-
-        // is corner point?
-		if lat>maxPt.lat {
-		    maxPt.lat= lat
-		}		
-		if lat<minPt.lat {
-		    minPt.lat= lat
+		if err != nil {
+			fmt.Printf("\n%d lat=%s\n", n, record[8])
+			panic("Data format error (lat)...")
 		}
-		
+
+		// is corner point?
+		if lat > maxPt.lat {
+			maxPt.lat = lat
+		}
+		if lat < minPt.lat {
+			minPt.lat = lat
+		}
+
 		// get longitude
 		long, err := strconv.ParseFloat(record[9], 64)
-        if err != nil {
-             panic("Data format error (long)...")
-        }
-		
-        // is corner point?
-		if long>maxPt.long {
-		    maxPt.long= long
-		}
-		
-		if long<minPt.long {
-		    minPt.long= long
+		if err != nil {
+			panic("Data format error (long)...")
 		}
 
-        // add point to the slice
+		// is corner point?
+		if long > maxPt.long {
+			maxPt.long = long
+		}
+
+		if long < minPt.long {
+			minPt.long = long
+		}
+
+		// add point to the slice
 		n++
-        pt:= GPScoord{lat,long}
-        coords= append(coords, LabelledGPScoord{pt,n,0})
-    }
+		pt := GPScoord{lat, long}
+		coords = append(coords, LabelledGPScoord{pt, n, 0})
+	}
 
-    return coords, minPt,maxPt
+	return coords, minPt, maxPt
 }
