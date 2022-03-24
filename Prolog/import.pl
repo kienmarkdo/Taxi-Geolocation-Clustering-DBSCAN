af70584a0ed102ae6364097bae1ddfe491e803e5
Kien Do <kienmarkdo@gmail.com>
2022-03-24 17:31:06 -0400

    Made starting template

--- Prolog/import.pl
+++ Prolog/import.pl
@@ -1,8 +1,10 @@
-import:-
-    csv_read_file('partition65.csv', Data65, [functor(partition)]),maplist(assert, Data65),
-    csv_read_file('partition74.csv', Data74, [functor(partition)]),maplist(assert, Data74),
-    csv_read_file('partition75.csv', Data75, [functor(partition)]),maplist(assert, Data75),
-    csv_read_file('partition76.csv', Data76, [functor(partition)]),maplist(assert, Data76),
-    csv_read_file('partition84.csv', Data84, [functor(partition)]),maplist(assert, Data84),
-    csv_read_file('partition85.csv', Data85, [functor(partition)]),maplist(assert, Data85),
-    csv_read_file('partition86.csv', Data86, [functor(partition)]),maplist(assert, Data86),listing(partition).
+import :-
+    csv_read_file('partition65.csv', Data65, [functor(partition)]), maplist(assert, Data65),
+    csv_read_file('partition74.csv', Data74, [functor(partition)]), maplist(assert, Data74),
+    csv_read_file('partition75.csv', Data75, [functor(partition)]), maplist(assert, Data75),
+    csv_read_file('partition76.csv', Data76, [functor(partition)]), maplist(assert, Data76),
+    csv_read_file('partition84.csv', Data84, [functor(partition)]), maplist(assert, Data84),
+    csv_read_file('partition85.csv', Data85, [functor(partition)]), maplist(assert, Data85),
+    csv_read_file('partition86.csv', Data86, [functor(partition)]), maplist(assert, Data86),
+    listing(partition) % partition(PARTITION_ID, POINT_ID, X, Y, CLUSTER_ID)
+.
---
+++ Prolog/main.pl
@@ -0,0 +1,27 @@
+/*
+Nom / Name             : Kien Do
+Cours / Course         : CSI2520 - Paradigmes de programation / Programming Paradigms
+Professeur / Professor : Robert Laganière, uottawa.ca
+Session                : Hiver 2022 / Winter 2022
+Projet / Project       : Merging DBSCAN-Clustered Taxi Geolocation Partitions
+*/
+
+/* ==============  Start program with "start."  ============== */
+start :-
+    consult("import.pl"),
+    import  % predicate in file import.pl
+.
+
+% Facts
+
+% Relations
+
+% Predicates/Rules
+
+/* ===============  Cluster merging algorithm  =============== */
+
+
+/* ===================  Helper predicates  =================== */
+% mergeClusters produces a list of all points with their cluster ID
+mergeClusters().
+
