a08714078371115b34f9b27af995196cf449421b
Kien Do <kienmarkdo@gmail.com>
2022-03-28 20:52:02 -0400

    cleaned up code

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -20,49 +20,10 @@ import :-
 
 
 
-% Facts
-clusterList([[]]).
 
-% Relations
-
-% Predicates/Rules
-
-/* ===============  Cluster merging algorithm  =============== */
-
-
-/* ===================  Helper predicates  =================== */
-% mergeClusters produces a list of all points with their cluster ID
-mergeClusters(L) :-
-    findall([D,X,Y,C], partition(_,D,X,Y,C), LL),
-    populateClusterList(LL)
-.
-
-% always start with adding to the empty cluster list
-populateClusterList([H|T]) :-
-    populateClusterList([H|T], add, [], ClusterList).
-
-% check to see if we should be relabelling or adding the current point to the cluster list
-populateClusterList([H|T], check, A, ClusterList) :-
-    notre_member(H, A),
-    populateClusterList([H|T], add, A, ClusterList),
-    !
-.
-populateClusterList([H|T], check, A, ClusterList) :-
-    not(notre_member(H, A)),
-    populateClusterList([H|T], relabel, A, ClusterList)
-.
-
-% ========= relabel / add to clusterList =========
-% relabel all labels of intersection points in A to label H
-populateClusterList([H|T], relabel, A, ClusterList) :-
-    relabel(A, H, AA)
-.
-populateClusterList([H|T], add, A, ClusterList) :-
-    insert(H, A, AA),
-    populateClusterList(T, check, AA, ClusterList)
-.
-
-% relabel works START
+% relabel/4
+% relabels the points of cluster OldClusterID with label NewClusterID
+% relabel(OldClusterID, NewClusterID, ClusterListIN, ClusterListOut)
 relabel(_, _, [], []) :- !.
 relabel(OldClusterID, NewClusterID, [[P, X, Y, OldClusterID]|T], Result) :-
     relabel(OldClusterID, NewClusterID, T, RR),
@@ -75,28 +36,10 @@ relabel(OldClusterID, NewClusterID, [[P, X, Y, NoChange]|T], Result) :-
     insert([P, X, Y, NoChange], RR, Result),
     !
 .
-% relabel works END
+% relabel END
 
-
-% loop through all the points (paritions == points)
-% findall([D,X,Y,C], partition(_,D,X,Y,C), L).
-
-% notre_member/2
-% notre_member(X, [_|L]) returns true if elements X is in list [_|L]
-notre_member(X,[X|_]).
-notre_member(X,[_|L]) :- notre_member(X,L).
-
-% make a list of 2 lists
-% combineTwoLists(L1, L2, [L1, L2]).
-
-% insert in front of list
+% insert/2
+% insert item in front of a list
+% insert(NewElement, List, NewList)
 insert(A, L, [A|L]).
-
-/*
-1. Loop through all the points ( partitions = points)
-2. Check if the points are in clusterlist
-3. If it is, relabel them
-4. If not, just add to your clusterlist
-5. Keep going till you hit the end
-*/
 
