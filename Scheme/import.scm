6cca12b41126335e0b9bea6484830aab63f45369
Kien Do <kienmarkdo@gmail.com>
2022-04-01 19:20:18 -0400

    almost donegit status Results slightly off (see description)
    results are slightly off compared to my Prolog results. Not sure if Prolog is correct or Scheme is correct. I'll try to match Scheme's to Prolog's

--- Scheme/import.scm
+++ Scheme/import.scm
@@ -21,16 +21,69 @@ Projet / Project       : Merging DBSCAN-Clustered Taxi Geolocation Partitions
 (define (import)
   (let ((p65 (readlist "partition65.scm"))
         (p74 (readlist "partition74.scm")) 
-        (p75 (readlist "partition75.scm")))
-    (append p65 p74 p75)))
+        (p75 (readlist "partition75.scm"))
+        (p76 (readlist "partition76.scm"))
+        (p84 (readlist "partition84.scm")) 
+        (p85 (readlist "partition85.scm"))
+        (p86 (readlist "partition86.scm"))
+        )
+    (append p65 p74 p75 p76 p84 p85 p86)))
 
 ; ====================================  Merge partition clusters  ==================================== ;
 
 (define (mergeClusters partitions)
-  (remove-partition-id partitions)
+  (define L (remove-partition-id partitions))
+  (populate-cluster-list L '())
+  ;(populate-cluster-list (remove-partition-id partitions) '())
 )
 
-; returns a list (POINT_ID, X, Y, CLUSTER_ID) given (PARTITION_ID, POINT_ID, X, Y, CLUSTER_ID)
+; populates an initially-empty cluster list by adding clusters to or relabelling clusters in cluster list
+; returns a list of non-overlapping clusters using tail-recursion
+(define (populate-cluster-list L AccL)
+  (cond
+    ((equal? L '()) (reverse AccL)) ; end case of tail-recursion; return reverse of AccL due to cons
+    ((equal? (overlap (car L) AccL) '()) (populate-cluster-list (cdr L) (cons (car L) AccL))) ; if no overlap
+    (else (relabel (last (car L)) (overlap (car L) AccL) AccL) (populate-cluster-list (cdr L) AccL)) ; if overlap
+  )
+)
+
+
+
+; relabels the points of current list of clusters having OldClusterID with NewClusterID
+(define (relabel old-id new-id L)
+  (cond
+    ((null? L) '())
+    ((= old-id (last (car L))) (cons (replace-last (car L) new-id) (relabel old-id new-id (cdr L))))
+    (else (cons (car L) (relabel old-id new-id (cdr L))))
+  )
+)
+; (relabel 33 77 '((1 2.2 3.1 33) (2 2.1 3.1 22) (3 2.5 3.1 33) (4 2.1 4.1 33) (5 4.1 3.1 30))) ; test case
+
+
+; returns NewClusterID if a given GPS point exists in the given cluster list
+; else, return null if the given point does not overlap with any points in cluster list
+; NOTE: Two points overlap if their PointIDs are the same; Assume that X and Y will also be the same
+(define (overlap X L)
+  (cond
+    ((null? L) '())
+    ((= (car X) (car (car L))) (list (last X)))
+    (else (overlap X (cdr L)))
+  )
+)
+;(overlap '(2 2.1 3.1 99) '((1 2.2 3.1 33) (2 2.1 3.1 22) (3 2.5 3.1 33) (4 2.1 4.1 33) (5 4.1 3.1 30))) ; 99
+;(overlap '(99 99 99 99) '((1 2.2 3.1 33) (2 2.1 3.1 22) (3 2.5 3.1 33) (4 2.1 4.1 33) (5 4.1 3.1 30))) ; false
+
+; returns a list where the last element is replaced with a given element
+; given (POINT_ID, X, Y, CLUSTER_ID) and NEW_ID, return (POINT_ID, X, Y, NEW_ID)
+; helper procedure for relabel procedure
+(define (replace-last L New-id)
+  (if (null? (cdr L))
+      (list New-id)
+      (cons (car L) (replace-last (cdr L) New-id)))) ; alternate: (append (list(car L)) (replace-last (cdr L) New-id))))
+
+
+
+; given a list of (PARTITION_ID, POINT_ID, X, Y, CLUSTER_ID), return a list of (POINT_ID, X, Y, CLUSTER_ID)
 ; removes PARTITION_ID parameter / returns a list without the PARTITION_ID parameter
 (define (remove-partition-id L)
   (cond
@@ -40,11 +93,20 @@ Projet / Project       : Merging DBSCAN-Clustered Taxi Geolocation Partitions
   )
 )
 
-(import)
+;(import)
+"===================================================="
 (mergeClusters (import))
+;(define list '((1 2.2 3.1 33) (2 2.1 3.1 22) (3 2.5 3.1 33) (4 2.1 4.1 33) (5 4.1 3.1 30) (1 2.2 3.1 100) (2 2.1 3.1 101) (3 2.5 3.1 102) (4 2.1 4.1 103)))
+;(mergeClusters list)
+;(overlap (car list) '((1 2.2 3.1 33) (2 2.1 3.1 22)))
 
 
 ; ==================================  Helper/auxiliary predicates  =================================== ;
 
 
 ; ==================================  Output results to text file  =================================== ;
+(define (saveList filename L)
+  (call-with-output-file filename
+    (lambda (out)
+      (write L out))))
+(saveList "kienClusters.txt" (mergeClusters (import)))
--- Scheme/testing.scm
+++ Scheme/testing.scm
@@ -9,3 +9,43 @@
 Temp1
 Temp2
 Combine
+
+; ======= thoughts while working on relabel procedure START =======
+; for relabel, there are several things we can do to update the last parameter CLUSTER_ID
+; we can make a new list without the last element then add NEW_CLUSTER_ID to the end of the new list
+;   we can reverse L, get the cdr, then reverse it again to remove the last element; append NEW_CLUSTER_ID to the end of that new list
+;   we can make a procedure to remove the last element from the list, then append NEW_CLUSTER_ID to that list
+
+; we could directly update the element at the last index, thereby directly modifying the original list and not needing to use any extra memory
+;   that recursion or tail-recursion would need. However, this is not "pure recursion" because how would we know when to stop? This is more
+;   of a solution for a for-loop.
+; ======= thoughts while working on relabel procedure END   =======
+
+; testing replace-last
+(define (replace-last L New-id)
+  (if (null? (cdr L))
+      (list New-id)
+      (cons (car L) (replace-last (cdr L) New-id)))) ; alternate: (append (list(car L)) (replace-last (cdr L) New-id))))
+(replace-last '(1 2 3) 9)
+
+; relabels the points of current list of clusters having OldClusterID with NewClusterID
+(define (relabel old-id new-id L)
+  (cond
+    ((null? L) '())
+    ((= old-id (last (car L))) (cons (replace-last (car L) new-id) (relabel old-id new-id (cdr L))))
+    (else (cons (car L) (relabel old-id new-id (cdr L))))
+  )
+)
+(relabel 33 77 '((1 2.2 3.1 33) (2 2.1 3.1 22) (3 2.5 3.1 33) (4 2.1 4.1 33) (5 4.1 3.1 30)))
+
+; test overlap
+(define (overlap X L)
+  (cond
+    ((null? L) '())
+    ((= (car X) (car (car L))) (list (last X)))
+    (else (overlap X (cdr L)))
+  )
+)
+(overlap '(2 2.1 3.1 99) '((1 2.2 3.1 33) (2 2.1 3.1 22) (3 2.5 3.1 33) (4 2.1 4.1 33) (5 4.1 3.1 30))) ; 99
+(overlap '(99 99 99 99) '((1 2.2 3.1 33) (2 2.1 3.1 22) (3 2.5 3.1 33) (4 2.1 4.1 33) (5 4.1 3.1 30))) ; ()
+(equal? '() (overlap '(99 99 99 99) '((1 2.2 3.1 33) (2 2.1 3.1 22) (3 2.5 3.1 33) (4 2.1 4.1 33) (5 4.1 3.1 30)))) ; true
